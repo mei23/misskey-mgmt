1b744b639c474742f0b470590d05e3187ed2169c
misskey <misskey@tsuki.network>
2019-04-26 20:11:24 +0900

    Add echo to track restart.sh status

--- restart.sh
+++ restart.sh
@@ -2,6 +2,7 @@
 
 /home/misskey/note 【メンテナンス告知】当インスタンスは、今から10秒後、約1分間 Misskey サービスの再起動を行います。その間、アクセスが円滑でないことがありますので、ご了承お願いいたします。
 
+echo
 echo wait 10 seconds ...
 sleep 10s
 
@@ -10,6 +11,7 @@ sudo systemctl stop misskey.service
 sudo systemctl start misskey.service
 sudo systemctl status --full --no-pager misskey.service
 
+echo wait for misskey.service bootup ...
 /home/misskey/wait-for-boot.sh
 
 /home/misskey/note 【メンテナンス終了】Misskey サービスの再起動が完了しました。
