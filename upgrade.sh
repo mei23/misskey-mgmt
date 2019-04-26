205cf73b0b953066ffadff8369360f63f1207f97
misskey <misskey@tsuki.network>
2019-04-26 20:31:15 +0900

    Fix nvm not found / copypaste problem

--- upgrade.sh
+++ upgrade.sh
@@ -8,10 +8,10 @@ git checkout $SERVICE_BRANCH > /dev/null 2>&1
 git status | grep "up to date"
 if [[ $? -eq 0 ]]; then exit 0; fi
 git reset --hard origin/$SERVICE_BRANCH
-nvm install $(cat .node-version)
-nvm use $(cat .node-version)
+. ~/.nvm/nvm.sh install $(cat .node-version)
+. ~/.nvm/nvm.sh use $(cat .node-version)
 
-MSKY_UPGRADE_VERSION=$(git describe --tags --exact-match || echo "$(git describe --tags $(git rev-list --tags --max-co unt=1)) ($(git rev-parse HEAD))")
+MSKY_UPGRADE_VERSION=$(git describe --tags --exact-match || echo "$(git describe --tags $(git rev-list --tags --max-count=1)) ($(git rev-parse HEAD))")
 /home/misskey/note 【メンテナンス告知】当インスタンスは、今から約10分間 Misskey $MSKY_UPGRADE_VERSION へのアップデートを行います。その間、アクセスが円滑でないことがありますので、ご了承お願いいたします。
 
 npm install -g npm
