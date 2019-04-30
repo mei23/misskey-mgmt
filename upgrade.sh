91e6464b89261e69799a57c0d38c836dc2edf6b3
Satsuki Yanagi <17376330+u1-liquid@users.noreply.github.com>
2019-04-30 13:14:07 +0900

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -2,6 +2,7 @@
 
 SERVICE_BRANCH=$(cat ~/.service-branch)
 
+export NODE_ENV=production
 pushd /home/misskey/live > /dev/null
 git fetch --all --tags --prune --prune-tags
 git checkout $SERVICE_BRANCH > /dev/null 2>&1
@@ -16,10 +17,12 @@ MSKY_UPGRADE_VERSION=$(git describe --tags --exact-match || echo "$(git describe
 /home/misskey/note 【メンテナンス告知】当インスタンスは、今から約10分間 Misskey $MSKY_UPGRADE_VERSION へのアップデートを行います。その間、アクセスが円滑でないことがありますので、ご了承お願いいたします。
 
 npm install -g npm
+npm install -g ts-node web-push
 npm run clean
 npm install
 NODE_ENV=production npm run build
 sudo systemctl stop misskey.service
+ts-node ./node_modules/typeorm/cli.js migration:run
 sudo systemctl start misskey.service
 popd > /dev/null
 
