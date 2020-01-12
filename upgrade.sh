39cbbb57d17baeb890ec185d791e3f123c3fe6e6
Satsuki Yanagi <17376330+u1-liquid@users.noreply.github.com>
2020-01-12 17:25:49 +0900

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -15,7 +15,7 @@ nvm install $(cat .node-version)
 nvm use $(cat .node-version)
 
 GIT_CURRENT_COMMIT=$(git rev-parse HEAD)
-MSKY_UPGRADE_VERSION=$(git describe --tags --exact-match || echo "$(git describe --tags $(git rev-list --tags --max-count=1)) (${GIT_CURRENT_COMMIT:0:8})")
+MSKY_UPGRADE_VERSION=$(git describe --tags --exact-match || echo "$(git describe --tags $(git rev-list --tags --max-count=1)) (${SERVICE_BRANCH_ORIGIN} @ ${GIT_CURRENT_COMMIT:0:8})")
 /home/misskey/note 【メンテナンス告知】当インスタンスは、今から約10分間 Misskey $MSKY_UPGRADE_VERSION へのアップデートを行います。その間、アクセスが円滑でないことがありますので、ご了承お願いいたします。
 git describe --tags --exact-match || sed -i -re '0,/"version":\s+".+"/ s/("version":\s+".+)"/\1-'"${SERVICE_BRANCH_ORIGIN}"'-'"${GIT_CURRENT_COMMIT:0:8}"'"/' package.json
 
